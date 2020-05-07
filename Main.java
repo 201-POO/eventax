0c36a44d5dd5256f9995b37c704a1071a8559256
Angel <asullom@gmail.com>
2020-05-07 09:55:39 -0500

    CRUD

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import java.util.List;
 import java.util.Scanner;
+
+import data.PersonData;
+import entities.Person;
 
 /**
  * Main
@@ -7,7 +11,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int opt;
+        int opt=0;
+        PersonData pData = new PersonData();
         do {
             System.out.println("***** CRUD PERSON *****");
             System.out.println("1 List ");
@@ -19,10 +24,29 @@ public class Main {
             switch (opt) {
                 case 1:
                     System.out.println("****** 1 List *****");
-                    
+                    List<Person> lis= pData.list();
+                    System.out.println("Name \t Sex");
+                    for(Person d: lis){
+                        System.out.println(d.getName()+"\t"+d.getSex());
+                    }
                     break;
                 case 2:
                     System.out.println("**** 2 New ****");
+                   
+                    System.out.print("Ingrese Name: ");
+                    String n= input.nextLine(); 
+                    input.nextLine();   
+                    System.out.print("Name: "+n);    
+                                            
+                    System.out.print("Ingrese Sex: ");
+                    String s= input.nextLine();
+                    input.nextLine();
+                    System.out.print("Sex: "+s);
+
+                    Person d = new Person();
+                    d.setName(n);
+                    d.setSex(s);
+                    pData.create(d);
                     break;
                 case 3:
                     System.out.println("By ");
--- data/PersonData.java
+++ data/PersonData.java
@@ -5,8 +5,11 @@ import java.util.List;
 import entities.Person;
 
 public class PersonData {
-    List<Person> list =new ArrayList<Person>();
+    static List<Person> lis =new ArrayList<Person>();
+    public List<Person> list() {
+        return lis;
+    }
     public void create(Person d) {
-        list.add(d);
+        lis.add(d);
     }
 }
