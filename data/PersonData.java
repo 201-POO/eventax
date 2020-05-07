44d8f2c071f33bf1ba03df78377a72abb39f12f3
Angel <asullom@gmail.com>
2020-05-07 11:13:44 -0500

    CRD ok

--- data/PersonData.java
+++ data/PersonData.java
@@ -1,36 +1,34 @@
 package data;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import entities.Person;
 
 public class PersonData {
-    List<Person> lis =new ArrayList<Person>();
-   // public static int id;
+    List<Person> lis = new CopyOnWriteArrayList<Person>();
+    // public static int id;
 
-    public PersonData(){
-   //     id=0;
+    public PersonData() {
+        // id=0;
     }
+
     public List<Person> list() {
         return lis;
     }
+
     public void create(Person d) {
-        int id = lis.size() +1;
-        System.out.println("ID:"+id);
+        int id = lis.size() + 1;
+        System.out.println("ID:" + id);
         d.setId(id);
         lis.add(d);
     }
+
     public void delete(int id) {
-        System.out.println("ID:"+id);
-        for(Person d: lis){
-            System.out.println("Deleted:"+d.getId() + "\t" + d.getName());
-            if (d.getId() == id){
-                try {
-                    lis.remove(d);
-                    System.out.println("Eliminado");
-                } catch (java.util.ConcurrentModificationException e2) {
-                    System.out.println("si exist e2="+e2);
-                }
+        for (Person d : lis) {
+            if (id == d.getId()) {
+                lis.remove(d);
+                System.out.println("Eliminado " + d.getName());
             }
         }
     }
