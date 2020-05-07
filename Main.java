08580d75fe57ce576155e4c605e1a9e97aaebf17
Angel <asullom@gmail.com>
2020-05-07 08:57:42 -0500

    Menu

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola");
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("***** CRUD PERSON *****");
+        System.out.println("1 List ");
+        System.out.println("2 New ");
+        System.out.println("0 Exit ");
+        System.out.println("Choice option: ");
+        int opt= input.nextInt();
+        System.out.println("You chosed: "+opt);
+        
+
     }
 }
