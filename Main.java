83129fb22b845477030352e0b269a3a7935b9c25
Angel <asullom@gmail.com>
2020-05-07 09:15:09 -0500

    Menu

--- Main.java
+++ Main.java
@@ -6,16 +6,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        int opt;
+        do {
+            System.out.println("***** CRUD PERSON *****");
+            System.out.println("1 List ");
+            System.out.println("2 New ");
+            System.out.println("0 Exit ");
+            System.out.println("Choice option: ");
+            opt = input.nextInt();
+            System.out.println("You chosed: " + opt);
+            switch (opt) {
+                case 1:
+                    System.out.println("****** 1 List *****");
+                    break;
+                case 2:
+                    System.out.println("**** 2 New ****");
+                    break;
+                case 3:
+                    System.out.println("By ");
+                    break;
+                default:
+                    System.out.println("Incorrecto");
+                    break;
+            }
 
-        System.out.println("***** CRUD PERSON *****");
-        System.out.println("1 List ");
-        System.out.println("2 New ");
-        System.out.println("0 Exit ");
-        System.out.println("Choice option: ");
-        int opt= input.nextInt();
-        System.out.println("You chosed: "+opt);
-        
+        } while (opt != 0);
 
     }
 }
