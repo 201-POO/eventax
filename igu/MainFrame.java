e44c5dbe9419813ec8753d34e84efdd49c6f65a8
Angel <asullom@gmail.com>
2020-05-08 11:32:12 -0500

    JFrame min

---
+++ igu/MainFrame.java
@@ -0,0 +1,11 @@
+package igu;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+    public MainFrame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,200);
+
+    }
+}
