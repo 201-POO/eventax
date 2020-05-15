2df8c681ee1cdfe6b0c70024e9e4fd58f11ff1d0
Angel <asullom@gmail.com>
2020-05-15 11:20:22 -0500

    intento de subir

--- App.java
+++ App.java
@@ -1,8 +1,11 @@
+import entities.Person;
 import igu.MainFrame;
 
 public class App {
 public static void main(String[] args) {
         MainFrame m= new MainFrame();
         m.setVisible(true);
+        Person person= new Person();
+        person.setId(5);
     }
 }
--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import entities.Person;
  * Main
  */
 public class Main {
-
+    //Este es el metodo Main
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int opt=0;
