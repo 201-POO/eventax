9ce33f538a1de78d3112f7f0e1a2ff8340db0425
Angel <asullom@gmail.com>
2020-05-15 12:05:35 -0500

    tarea 1000 int1

--- data/PersonData.java
+++ data/PersonData.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import entities.Person;
 
 public class PersonData {
+    // si ok
     static List<Person> lis = new CopyOnWriteArrayList<Person>();
     static int id=0;
 
