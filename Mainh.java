5c53e62d1d3681556ca40dec9daff83d52b73098
Sullon <asullom@upeu.edu.pe>
2020-05-28 10:15:36 -0500

    herencia Customer extends Person

--- Mainh.java
+++ Mainh.java
@@ -1,5 +1,25 @@
+//import data.PersonData;
+import entities.Person;
+import entities.Customer;
+
 public class Mainh {
     public static void main(String[] args) {
-        System.out.println("Hola H");
+        System.out.println("Hola Herencia");
+        System.out.println("============Customer SHOW============");
+        Customer c= new Customer();
+        c.eat();
+        c.setName("Empresa 1");
+        System.out.println("Customer name:"+c.getName());
+        c.setTaxpayerId("RUC 10256398459");
+        System.out.println("Customer tax:"+c.getTaxpayerId());
+        c.receivePay();
+
+        System.out.println("============Person SHOW============");
+        Person p= new Person();
+        p.eat();
+        p.setName("Persona 1");
+        System.out.println("Person name:"+p.getName());
+        //p.setTaxpayerId("RUC 10256398459");
+        //System.out.println("Person tax:"+p.getTaxpayerId());
     }
 }
